bed83a2bc14830f7f484406b75875718bea7a970
Deyan Peychev <deyan.p.peychev@gmail.com>
2017-11-24 20:19:49 +0200

    Added Tour of Heroes Angular tutorial and started developing final project assignment.

---
+++ Angular Fundamentals/exercises/00-Tour-Of-Heroes-Intro/ng/src/app/hero.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {Hero} from './heroes/Hero';
+import {HEROES} from './mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable()
+export class HeroService {
+
+  constructor(private messageService: MessageService) {
+  }
+
+  getHeroes(): Observable<Hero[]> {
+    // Todo: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+}
